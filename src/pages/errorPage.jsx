3c7d27a3d58b014cb57c7770bfa56ec8d17bfd0a
Octavian Pashya Ramadhan <107595390+Pashyaaaa@users.noreply.github.com>
2024-10-10 16:17:51 +0700

    <fix>(errorPage):{Perbaiki Bug Font}

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <div className="w-full min-h-screen flex justify-center items-center gap-5 flex-col">
       <h1 className="text-3xl font-bold">Oops!</h1>
-      <p className="my-5 text-xl">Sorry, Unexpected Error</p>
+      <p className="my-5 text-xl font-semibold">Sorry, Unexpected Error.</p>
       <p className="text-lg">{error.statusText || error.message}</p>
     </div>
   );
